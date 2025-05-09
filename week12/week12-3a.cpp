a517bbafbc99fe1cb768e169966663bc8829a707
yuanhsihsu <yuanhsi0727@gmail.com>
2025-05-09 11:48:53 +0800

    week12

---
+++ week12/week12-3a.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+
+	int s;
+	scanf("%d", &s);
+	int ans = 1.2*60*60/s;
+	printf("%d", ans);
+
+
+}
---
+++ week12/week12-3b.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+
+	int y;
+	scanf("%d", &y);
+	if(y%400==0) printf("%d is a leap year.", y);
+	else if(y%100==0) printf("%d is not a leap year", y);
+	else if(y%4==0) printf("%d is a leap year.\n", y);
+	else printf("%d is not a leap year.\n", y);
+
+
+}
